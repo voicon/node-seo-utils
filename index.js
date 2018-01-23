636c3520e047aacb18717d2e3c9553291340d568
Kelvin Tran <xuanvinh@gmail.com>
2018-01-23 13:13:13 +0700

    - completed and add tests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const rule = require('./src/libs/rule');
 const reader = require ('./src/libs/reader');
 const writer = require ('./src/libs/writer');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 /*
 $ = cheerio.load('<h2 class = "title">Hello world</h2><meta name="keywords abc" /> <meta name="robots" />');
@@ -30,7 +31,10 @@ console.log(reader.getDoc());*/
 var manager = new mag();
 
 manager.setReader(new reader.ReaderFile('./test/index.html'));
-manager.setWriter(new writer.WriterConsole());
+//manager.setWriter(new writer.WriterConsole());
+//manager.setWriter(new writer.WriterFile('./test/out'));
+var wstream = fs.createWriteStream('./test/out1');
+manager.setWriter(new writer.WriterStream(wstream));
 
 manager.addRule(new rule.RuleRootExistTag('head', 'meta'));
 manager.addRule(new rule.RuleRootExistTag('head', 'title'));
@@ -42,7 +46,7 @@ manager.addRule(new rule.RuleTagExistAttribute('', 'address', 'name'));
 manager.addRule(new rule.RuleTagAttributeExistValue('head', 'meta', 'name', 'keywords'));
 manager.addRule(new rule.RuleTagAttributeExistValue('', 'a', 'href', 'index'));
 
-manager.addRule(new rule.RuleTagCount('', 'li', 5));
+manager.addRule(new rule.RuleTagCount('', 'li', 3));
 
 
 manager.check();
--- src/libs/writer.js
+++ src/libs/writer.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 function Writer() {
     this.data = '';
 }
@@ -6,7 +8,11 @@ Writer.prototype.setData = function(data) {
     this.data = data;
 }
 
-Writer.prototype.write = function() { }
+Writer.prototype.write = function(data) { 
+    if(data !== undefined && data != "") {
+        this.setData(data);
+    }
+}
 
 WriterConsole.prototype = new Writer();
 WriterConsole.prototype.constructor = WriterConsole;
@@ -16,8 +22,52 @@ function WriterConsole() {
 }
 
 WriterConsole.prototype.write = function(data) {
-    this.setData(data);
+    Writer.prototype.write.call(this, data);
     console.log(this.data);
 }
 
-module.exports = {Writer: Writer, WriterConsole: WriterConsole};
+WriterFile.prototype = new Writer();
+WriterFile.prototype.constructor = WriterFile;
+
+function WriterFile(filePath) {
+    Writer.call(this);
+    this.filePath = filePath;
+}
+
+WriterFile.prototype.write = function(data) {
+    Writer.prototype.write.call(this, data);
+
+    fs.writeFile(this.filePath, this.data, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+}
+
+WriterStream.prototype = new Writer();
+WriterStream.prototype.constructor = WriterStream;
+
+function WriterStream(oStream) {
+    Writer.call(this);
+    this.stream = oStream;
+}
+
+WriterStream.prototype.write = function(data) {
+    Writer.prototype.write.call(this, data);
+
+    try {
+        this.stream.write(this.data);
+        // on Node.js older than 0.10, add cb to end()
+        this.stream.end(function () { console.log('Write stream done'); });
+    } catch(e) {
+        console.log('Write stream fail');
+    }
+}
+
+module.exports = {
+    Writer: Writer, 
+    WriterConsole: WriterConsole,
+    WriterFile: WriterFile,
+    WriterStream: WriterStream
+};
